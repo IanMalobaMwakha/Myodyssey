63b6074ec13b7a0c59ac8331a0af6e8bc9611359
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-08-27 16:04:29 +0300

    modified:   myodyssey/myodyssey/urls.py

--- myodyssey/myodyssey/urls.py
+++ myodyssey/myodyssey/urls.py
@@ -1,10 +1,7 @@
-"""
-URL configuration for myodyssey project.
-
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(blog.urls))
+    path('', include(blog.urls)),
 ]
