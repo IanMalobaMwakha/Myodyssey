6373119e01f865b48a06cc51bc768dcc29577340
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-08-28 14:13:21 +0300

    modified:   myodyssey/blog/__pycache__/urls.cpython-310.pyc

--- myodyssey/blog/views.py
+++ myodyssey/blog/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 #def home(request):
@@ -16,3 +16,7 @@ class ArticleDetailView(DetailView):
     template_name  = "article_detail.html"
 
 
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = "__all__"
