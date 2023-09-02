06e086bb1deab8a992edef2c2b92bc211b820ff9
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-09-02 17:14:59 +0300

    modified:   myodyssey/blog/forms.py

--- myodyssey/blog/forms.py
+++ myodyssey/blog/forms.py
@@ -4,10 +4,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fileds = ('title', 'author', 'body')
+        fields = ('title', 'author', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
